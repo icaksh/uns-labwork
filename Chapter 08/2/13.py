d56b661852b39ab12b0fe6a44ca88b815ba8004d
icaksh <me@icaksh.my.id>
2020-11-24 09:41:49 +0000

    must be function ._.

--- Chapter 08/2/13.py
+++ Chapter 08/2/13.py
@@ -1,17 +1,23 @@
+def nilaiTertinggi(nilaiMhs):
+    nilaiDewa = 0
+    for i in nilaiMhs:
+        nilaiMid = i.get('mid')
+        nilaiFin = i.get('uas')
+        nilaiAkhir = (nilaiMid + 2*nilaiFin)/3
+        if(nilaiAkhir>nilaiDewa):
+            nilaiDewa = nilaiAkhir
+            data = {}
+            data['nim'] = i.get('nim')
+            data['nama'] = i.get('nama')
+    return data
+
 nilaiMhs = [{'nim' : 'A01', 'nama' : 'Amir', 'mid' : 50, 'uas' : 80},
 {'nim' : 'A02', 'nama' : 'Budi', 'mid' : 40, 'uas' : 90},
 {'nim' : 'A03', 'nama' : 'Cici', 'mid' : 50, 'uas' : 50},
 {'nim' : 'A04', 'nama' : 'Dedi', 'mid' : 20, 'uas' : 30},
 {'nim' : 'A05', 'nama' : 'Fifi', 'mid' : 70, 'uas' : 40}]
 
-nilaiDewa = 0
-for i in nilaiMhs:
-    nilaiMid = i.get('mid')
-    nilaiFin = i.get('uas')
-    nilaiAkhir = (nilaiMid + 2*nilaiFin)/3
-    if(nilaiAkhir>nilaiDewa):
-        nilaiDewa = nilaiAkhir
-        getNumber = i.get('nim')
-        getNama = i.get('nama')
+nilaiTinggi = nilaiTertinggi(nilaiMhs)
+print("Mahasiswa dengan nama {0} dan NIM {1} mendapatkan nilai akhir tertinggi".format(nilaiTinggi['nama'],nilaiTinggi['nim']))
 
-print("Mahasiswa dengan nama {0} dan NIM {1} mendapatkan nilai akhir tertinggi".format(getNama,getNumber))
+
